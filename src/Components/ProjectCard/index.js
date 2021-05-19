47f2b0f6f981c9d349b2e04306c7a41aec7c4ed4
theresfuelinmycoffee <atigrouse.com>
2021-05-19 15:48:28 -0700

    first commit

---
+++ src/Components/ProjectCard/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from '../../assets/318x180.svg';
+import style from './styles.module.css';
+
+//let image = placeholder;
+let title = "Card Title";
+let body =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+const ProjectCard = (props, onClick) => {
+  return (
+    <div className={style.main}>
+      <div className={style.ImageContainer}>
+        <img src={Image} />
+      </div>
+
+      <div className={style.cardContent}>
+        <div className={style.cardTitle}>
+          <h1>{props.title}</h1>
+        </div>
+
+        <div className={style.cardDescription}>
+          <p>{props.body}</p>
+        </div>
+
+        <div className={style.btn}>
+          <button onClick={console.log("clicked")}>
+            <a>See Full Project</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./styles.module.css";
+import logo from "./assets/website_logo_transparent_background.png";
+import ProjectCard from "./Components/ProjectCard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
+let marketdesciption = "A place where you can buy, sell, or trade your gemtly used pet supplies with others";
+let puppysocialdescription = "Have you wanted to socialize your puppy but needed help finding a friend? Find other puppy owners in your city.";
+let petconnectdescription = "Going out of town and your pet has special needs? use this tool to help your caretaker understand what your pet needs.";
+const App = () => {
+  return (
+    <div className={styles.homepageContainer}>
+      <div className={styles.homepageHeader}>
+        <div>
+          <img src={logo} alt="kennel-trader-logo" />
+        </div>
+      </div>
+      <div className={styles.homepageBody}>
+        <h1>Coming Soon!</h1>
+        <h3>
+          Kennel Trader is dedcated to making help everyone provide the best
+          life for thier pets! Here is some of whats to come:
+        </h3>
+      </div>
+      <div className={styles.cardContainer}>
+        <ProjectCard title="Market Place" body={marketdesciption} />
+        <ProjectCard title="Puppy Social" body={puppysocialdescription} />
+        <ProjectCard title="Pet Connect" body={petconnectdescription} />
+      </div>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Take the component and shoe iut on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
